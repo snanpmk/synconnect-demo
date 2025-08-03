c166d6199f2ce00641d6bcc9ea272dc72b925980
Sinan Pmk <sinan@finnest.io>
2025-08-03 14:15:09 +0530

    feature: status secton

--- src/pages/client/landing/LandingPage.jsx
+++ src/pages/client/landing/LandingPage.jsx
@@ -1,11 +1,13 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Status from "./components/Status";
 
 const LandingPage = () => (
   <>
     <Navbar />
     <Hero />
+    <Status />
     <About />
   </>
 );
